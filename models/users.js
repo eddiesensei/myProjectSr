ce3623a61e5970a639bbce5550e3952d13efb077
eddiesensei <edgardor8@outlook.com>
2023-03-17 01:43:23 -0400

    added last name form to registration page

--- models/users.js
+++ models/users.js
@@ -1,7 +1,8 @@
 const mongoose = require ('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    firstname: String,
+    lastname: String,
     email: {
         type: String,
         required: true,
