fd944cb5dc47abd4c9ec6d50fcb52bb6ec25980f
eddiesensei <edgardor8@outlook.com>
2023-03-17 15:50:46 -0400

    added login post route to allow users entry to homepage

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/users')
+
 
 router.get('/', (req, res) => {
     console.log('Successful login launch')
     res.render('login');
 })
 
-router.post('/', (req, res) => {
-     
+router.post('/', async (req, res) => {
+
+    try{
+            const check = await User.findOne({ email: req.body.email })
+
+            if(check.password === req.body.password){
+                res.render('index')
+            }
+            else{
+                res.send('Incorrect Email or Password')
+            }
+
+
+    }catch (e){
+            console.log(e)
+            res.send('Incorrect Email or Password')
+    }
+
 })
 
 module.exports = router
